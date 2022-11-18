b32c68942cf7f46b2d0e2db31fd5af367d9259e6
Shuu <ghwls328@naver.com>
2022-11-18 23:08:42 +0900

    NM과 K(1)

---
+++ 18290.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace BaekjoonCs
+{
+    class _18290
+    {
+        private static StreamReader sr = new StreamReader(Console.OpenStandardInput());
+        private static StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+        private static int n, m, k;
+        private static int[,] a;
+        private static bool[,] visited;
+        private static int answer;
+        private static int c;
+
+        private static void Search(int count, int val)
+        {
+            if (count == k)
+            {
+                if (answer < val) answer = val;
+                return;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (Visitable(i, j))
+                    {
+                        visited[i, j] = true;
+                        Search(count + 1, val + a[i, j]);
+                        visited[i, j] = false;
+                    }
+                }
+            }
+        }
+
+        private static bool Visitable(int i, int j)
+        {
+            if (visited[i, j]) return false;
+            if ((i > 0) && visited[i - 1, j]) return false;
+            if ((i < n - 1) && visited[i + 1, j]) return false;
+            if ((j > 0) && visited[i, j - 1]) return false;
+            if ((j < m - 1) && visited[i, j + 1]) return false;
+
+            return true;
+        }
+        public static void Solution()
+        {
+            int[] nmk = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+            n = nmk[0];
+            m = nmk[1];
+            k = nmk[2];
+
+            a = new int[n, m];
+            visited = new bool[n, m];
+            answer = -99999;
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] line = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+                for (int j = 0; j < m; j++)
+                {
+                    a[i, j] = line[j];
+                }
+            }
+
+            Search(0, 0);
+            sw.WriteLine(answer);
+            sw.Close();
+            sr.Close();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,84 +5,11 @@ using System.IO;
 
 namespace BaekjoonCs
 {
-    class _6064
-    {
-        private static StreamReader sr = new StreamReader(Console.OpenStandardInput());
-        private static StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
-
-        private static int Kaing(int m, int n, int x, int y)
-        {
-            int year = x - 1;
-            int max = LCM(m, n);
-
-            x = 1;
-            y -= year;
-            while (y < 1) y += n;
-
-            year += 1;
-            while (y != 1)
-            {
-                y -= m;
-                year += m;
-                while (y < 1) y += n;
-                if (year > max)
-                {
-                    year = -1;
-                    break;
-                }
-            }
-            return year;
-        }
-
-        private static int GCD(int a, int b)
-        {
-            if (a < b)
-            {
-                int tmp = a;
-                a = b;
-                b = tmp;
-            }
-
-            do
-            {
-                int remain = a % b;
-                a = b;
-                b = remain;
-            }
-            while (b > 0);
-
-            return a;
-        }
-
-        private static int LCM(int a, int b)
-        {
-            return a * b / GCD(a, b);
-        }
-
-        public static void Solution()
-        {
-            int t = int.Parse(sr.ReadLine());
-
-            for (int i = 0; i < t; i++)
-            {
-                int[] mnxy = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
-                int m, n, x, y;
-                m = mnxy[0];
-                n = mnxy[1];
-                x = mnxy[2];
-                y = mnxy[3];
-
-                sw.WriteLine(Kaing(m, n, x, y));
-            }
-            sw.Close();
-            sr.Close();
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            _6064.Solution();
+            _18290.Solution();
         }
     }
 }
